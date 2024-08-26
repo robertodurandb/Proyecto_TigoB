dcc001829e7dd4c0cd8621b72ea4000d0c17bce9
RobertoD <mitchelldurandb25@gmail.com>
2024-08-26 02:08:51 -0500

    images

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -6,22 +6,20 @@ const storage = multer.diskStorage({
     cb(null, 'uploads/'); // Directorio donde se guardarán las imágenes
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname); // Nombre único para el archivo
-    console.log(file);
+    cb(null, Date.now() + '-' + file.originalname) // Nombre único para el archivo
   }
-});
-let upload = multer({ storage: storage });
-
-const newupload = upload.single('image');
-
+})
+let upload = multer({ storage: storage })
+const newupload = upload.single('image')
 // Ruta para subir imágenes
 
 const uploadfile = (req, res) => {
+
 req.getConnection((err, conn)=>{
    if(err) return res.send(err)
 
        const tipo = req.file.mimetype
-       const nombreimg = Date.now() + '-' + req.file.originalname
+       const nombreimg = req.file.filename
 
        conn.query('INSERT INTO imagen set ?', 
          [{tipo, nombreimg}], (err, rows)=>{
@@ -29,13 +27,15 @@ req.getConnection((err, conn)=>{
                return res.status(401).send(err)
            }else{
                let idimag = rows.insertId;
-               res.status(200).send('Imagen added, '+idimag)             
+               res.status(200).send('Imagen added, '+idimag)  
+              console.log(req.file.filename)
            }
           
        })
 
 })
 }
+
 
 export {newupload, uploadfile}
 
